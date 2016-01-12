a1a0dbe1e51a9600e3238ba8680b45134085eb04
Janne Hyvönen <Janne.j.hyvonen@gmail.com>
2016-01-12 23:53:13 +0200

    Project folders created, db created, config file created. On page 127.

---
+++ project/_config.py
@@ -0,0 +1,13 @@
+import os
+
+#Grab the folder where this script lives
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+DATABASE = 'flasktaskr.db'
+USERNAME = 'admin'
+PASSWORD = 'admin'
+WTF_CSRF_ENABLED = True
+SECRET_KEY = '/O@{L/FK+-aI^Cg9cDa56Y7Qdjl01t'
+
+#define the full path for database
+DATABASE_PATH = os.path.join(basedir, DATABASE)
