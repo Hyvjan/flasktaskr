03ee0d4b3f3756beba26f4e43d060d1ec96bc4c2
Janne Hyvönen <Janne.j.hyvonen@gmail.com>
2016-01-23 23:20:08 +0200

    models.py updated, hardcoded username and password removed from _config.py.Need to add the form validation classes. On page 159.

--- project/_config.py
+++ project/_config.py
@@ -4,8 +4,6 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 DATABASE = 'flasktaskr.db'
-USERNAME = 'admin'
-PASSWORD = 'admin'
 WTF_CSRF_ENABLED = True
 SECRET_KEY = '/O@{L/FK+-aI^Cg9cDa56Y7Qdjl01t'
 
