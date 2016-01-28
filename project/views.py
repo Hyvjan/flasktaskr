6b60676c54042c2a28b26a00ab7176621ad56c08
Janne Hyvönen <Janne.j.hyvonen@gmail.com>
2016-01-28 23:45:04 +0200

    On page 171. Need to update tasks.html.

--- project/views.py
+++ project/views.py
@@ -4,6 +4,7 @@ from flask import Flask, flash, redirect, render_template, \
 	request, session, url_for
 from forms import AddTaskForm, RegisterForm, LoginForm
 from flask.ext.sqlalchemy import SQLAlchemy
+import datetime
 
 # config
 
@@ -76,6 +77,8 @@ def new_task():
 				form.name.data,
 				form.due_date.data,
 				form.priority.data,
+				datetime.datetime.utcnow(),
+				'1',
 				'1'
 			)
 			db.session.add(new_task)
